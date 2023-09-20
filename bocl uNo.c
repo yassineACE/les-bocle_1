1a3273d30b95a7f384a4911f5bcd95453fc36b02
yassineACE <yassinemedmoum453@gmail.com>
2023-09-20 15:24:46 +0100

    first n1

---
+++ bocl uNo.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+int r , i ;
+        r = 0;
+for    (i=1;i<=5;i++){
+    r=r+i;
+}
+printf("LE SOMME DE 5 PREMIER ENTIER EST:%d",r);
+      return 0;
+}
